fe79a7ce3ac38d2c811844f09ce8a2b6aef25ab1
disster <p.tolparov@gmail.com>
2020-03-10 21:35:13 +0300

    added services and advantages

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,27 @@ $().ready(function() {
     $(this).toggleClass("burger-menu-button-active");
     $(".burger-menu").toggleClass("burger-menu-active");
   });
-  let small_icon =
-    "<svg class='card__icon'> <use xlink:href='images/sprite.svg#map'></use></svg>";
-
-  $(".services__card").each(function() {
-    if ($(this).hasClass("services__card-icon_small")) {
-      $(this).append(small_icon);
+  let map_icon =
+    '<div class="map-label"></div><div class="map-label__icon"><svg class="map-label__svg"><use xlink:href="images/sprite.svg#map"></use></svg></div><div class="map-label__text">Смотреть карту территорий</div>';
+  $(".card").each(function() {
+    if ($(this).hasClass("card-map-icon")) {
+      $(this).append(map_icon);
     }
   });
+  $(".map-label").hover(
+    function() {
+      $(this).css("width", "80%");
+      $(this)
+        .parent()
+        .find(".map-label__text")
+        .css("left", "55px");
+    },
+    function() {
+      $(this)
+        .parent()
+        .find(".map-label__text")
+        .css("left", "-100%");
+      $(this).css("width", 55);
+    }
+  );
 });
