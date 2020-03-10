9e8c2fa60d862b524cce773ebf7eac3f11cde194
disster <p.tolparov@gmail.com>
2020-03-10 15:00:20 +0300

    c

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,12 @@ $().ready(function() {
     $(this).toggleClass("burger-menu-button-active");
     $(".burger-menu").toggleClass("burger-menu-active");
   });
+  let small_icon =
+    "<svg class='card__icon'> <use xlink:href='images/sprite.svg#map'></use></svg>";
+
+  $(".services__card").each(function() {
+    if ($(this).hasClass("services__card-icon_small")) {
+      $(this).append(small_icon);
+    }
+  });
 });
